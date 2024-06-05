5bd5431233a181a152c0e5af84eac077e20d18bd
MCSNicacio <n218335@dac.unicamp.br>
2024-06-05 12:06:54 -0300

    Create ProjMetodosB.py

---
+++ ProjMetodosB.py
@@ -0,0 +1,27 @@
+### Projeto B Métodos 
+# Nicácio de Mattos(RA:218335) e Vitor de Oliveira (RA:206649)
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+#n_i = número de iterações
+#N = tamanho da imagem
+def Mandelbrot(n_i, N):
+
+    x_in = np.linspace(-2, 2, N)
+    y_in = np.linspace(-2, 2, N)
+    [a, b] = np.meshgrid(x_in, y_in * 1j)
+    z = a+b
+    c =a+b
+    div_array = np.zeros([N,N])
+   
+    for p in range(n_i):
+        div_ind = np.abs(z) < 2
+        div_array[div_ind] = div_array[div_ind]+1
+        z= z**2 + c
+
+    plt.figure(figsize = (10, 10))
+    plt.pcolormesh(x_in, y_in, div_array, cmap = "BuPu")
+
+Mandelbrot(100,1000)
+    
